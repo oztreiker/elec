6ce9511b33cbe536790de07a6f3546db9fde58fe
Rodrigo <oztreiker@gmail.com>
2014-09-16 15:42:55 -0400

    Routing con Angularjs

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+//creacion del modulo
+var angularRoutingApp = angular.module('angularRoutingApp',['ngRoute']);
+
+//configuracion de la rutas
+angularRoutingApp.config(function ($routeProvider) {
+	$routeProvider
+		.when('', {
+			templateUrl: '/index.html',
+			controller: 'mainController'
+		})
+		.when('/mision', {
+			templateUrl: 'view/mision.html',
+			controller: 'misionController'
+		})
+		.otherwise({ 
+			redirectTo: '/' 
+		});
+});
+
+angularRoutingApp.controller('mainController', function($scope) {
+	$scope.message = 'Hola, Mundo!';
+});
+
+angularRoutingApp.controller('misionController', function($scope) {
+	$scope.message = 'Esta es la página "mision de la empresa"';
+});
+
+angularRoutingApp.controller('contactController', function($scope) {
+	$scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
+});
